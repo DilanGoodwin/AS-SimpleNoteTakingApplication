63cded994ba3c23422ba7269d249ebda2e3bae66
Dilan Goodwin <dilanjgoodwin@yahoo.co.uk>
2025-02-08 10:05:07 +0000

    001: Created basic application outline with user text area and previews for different UI mode views.

--- app/src/main/java/com/learning/simplenotetakingapplication/MainActivity.kt
+++ app/src/main/java/com/learning/simplenotetakingapplication/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.learning.simplenotetakingapplication
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.learning.simplenotetakingapplication.ui.theme.SimpleNoteTakingApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +31,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SimpleNoteTakingApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()){innerPadding->
+                    TextArea(modifier=Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +40,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TextArea(modifier:Modifier=Modifier){
+    var text by remember{mutableStateOf("")}
+
+    Column(horizontalAlignment=Alignment.CenterHorizontally,modifier=modifier){
+        Text("Enter Note",fontWeight=FontWeight.Bold,fontSize=20.sp)
+        TextField(value=text,onValueChange={text=it},modifier=Modifier.fillMaxWidth().padding(10.dp))
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(name="LightMode",showBackground = true)
+@Preview(name="DarkMode",uiMode= Configuration.UI_MODE_NIGHT_YES,showBackground=true)
+annotation class SmallAreaPreview
+
+@Preview(name="LightModeFull",uiMode=Configuration.UI_MODE_NIGHT_NO,showSystemUi=true)
+@Preview(name="DarkModeFull",uiMode=Configuration.UI_MODE_NIGHT_YES,showSystemUi=true)
+annotation class FullAreaPreview
+
+@SmallAreaPreview
 @Composable
 fun GreetingPreview() {
     SimpleNoteTakingApplicationTheme {
-        Greeting("Android")
+        TextArea()
+    }
+}
+
+@FullAreaPreview
+@Composable
+fun FullNotePreview() {
+    SimpleNoteTakingApplicationTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()){innerPadding->
+            TextArea(modifier=Modifier.padding(innerPadding))
+        }
     }
 }
